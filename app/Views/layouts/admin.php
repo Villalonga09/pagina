fadbbbb1503d002f98c567fc80873346aa26166f
Villalonga09 <villalongarichard0410@gmail.com>
2025-08-20 09:24:45 -0400

    feat: dynamic admin logo

--- app/Views/layouts/admin.php
+++ app/Views/layouts/admin.php
@@ -1,6 +1,9 @@
 <?php
 require_once APP_PATH.'/Core/Auth.php';
+require_once APP_PATH . "/Models/Setting.php";
 $user = Auth::user() ?? ['name' => 'Admin'];
+$__s = new Setting();
+$__logo = $__s->get('site_logo', '');
 ?>
 <!doctype html>
 <html lang="es">
@@ -12,7 +15,7 @@ $user = Auth::user() ?? ['name' => 'Admin'];
 <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gray-900 text-gray-100">
-  <aside id="admin-sidebar" data-user='<?= json_encode($user) ?>' class="peer group fixed left-0 top-0 h-screen w-20 hover:w-64 bg-gray-900 border-r border-gray-800 transition-all duration-300 overflow-hidden flex flex-col"></aside>
+  <aside id="admin-sidebar" data-user='<?= json_encode($user) ?>' data-logo='<?= Utils::e($__logo) ?>' class="peer group fixed left-0 top-0 h-screen w-20 hover:w-64 bg-gray-900 border-r border-gray-800 transition-all duration-300 overflow-hidden flex flex-col"></aside>
   <main class="ml-20 p-4 transition-all duration-300 peer-hover:ml-64">
     <div class="container">
       <?= $content ?>
